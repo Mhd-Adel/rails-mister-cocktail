a3f86222ad074da3f57397fe358869e64d3fc4be
Mhd Adel Al Masri <mhdadelalmasri@gmail.com>
2021-02-18 16:22:10 +0100

    added the routes , controlles , view

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+before_action :find_cocktail, only: [ :show ]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to  new_cocktail_dose_path(@cocktail)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def doses_params
+    params.require(:dose).permit(:description)
+  end
+
+  def find_doses
+  end
+end
